4a106282199fb2461db1fefe207ae9ca590f648b
gatikrajput <gatikrajput08@gmail.com>
2023-10-05 01:01:39 +0530

    update port

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.use(express.json());
 
 //load config from env file
 require("dotenv").config();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 6010;
 
 //mount the API routes
 app.use("/api/v1", CountryRoutes);
